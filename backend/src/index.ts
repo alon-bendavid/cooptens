767d541cc41edade72475a33f6b9426898c528c9
alon-bendavid <114388870+alon-bendavid@users.noreply.github.com>
2024-10-08 12:05:11 +0200

    relunch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import { Context } from "./types";
-
+// backend
 const app = express();
 const httpServer = http.createServer(app);
 
